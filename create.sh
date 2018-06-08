b112c0ac383d8694af59a0a5ace09c7031ac7449
Bastien Paul <bastien.paul@coppernic.fr>
2018-06-08 10:33:09 +0200

    [BUILD] Update package list

---
+++ create.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# build and tag
+docker build -f ubuntu/standalone/Dockerfile -t android .
+docker tag android coppernic/docker-android-sdk:184.28
+
+# Login to docker hub
+docker login
+
+# Push image to docker hub
+docker push coppernic/docker-android-sdk:184.28
